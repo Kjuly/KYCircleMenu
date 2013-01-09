1ad3c04f59af143a5200feca5373fb065c1c3b0d
Kjuly <kj@kjuly.com>
2013-01-09 09:50:48 +0800

    Modify Constants.h

--- KYCircleMenuDemo/KYCircleMenuDemo/Constants.h
+++ KYCircleMenuDemo/KYCircleMenuDemo/Constants.h
@@ -1,6 +1,6 @@
 //
-//  KYConstants.h
-//  KYProjectTemplate
+//  Constants.h
+//  KYCircleMenuDemo
 //
 //  Created by Kaijie Yu on 6/24/12.
 //  Copyright (c) 2012 Kjuly. All rights reserved.
@@ -17,32 +17,20 @@
 
 #pragma mark -
 #pragma mark - View  - prefix: KY
-// Original UI Size
-#define kKYKeyboardHeight 216.f
 
 // App View Basic
-#define kKYViewHeight 460.f
-#define kKYViewWidth  320.f
-
-#define kKYContentMargin 10.f
-#define kKYContentWidth  300.f
+#define kKYViewHeight CGRectGetHeight([UIScreen mainScreen].applicationFrame)
+#define kKYViewWidth  CGRectGetWidth([UIScreen mainScreen].applicationFrame)
 
 // Navigation Bar
 #define kKYNavigationBarHeight 44.f
-
-#define kKYNavigationBarBackButtonHeight 44.f
-#define kKYNavigationBarBackButtonWidth  44.f
-#define kKYNavigationBarBackButtonArrowScale .5f
-#define kKYNavigationBarBackButtonArrowThick 3.f
 
 // Button Size
 #define kKYButtonInMiniSize   16.f
 #define kKYButtonInSmallSize  32.f
 #define kKYButtonInNormalSize 64.f
 
-// Root View
 
-
-@interface KYConstants : NSObject
+@interface Constants : NSObject
 
 @end
